13bae2041782d9ac3235412355d2f8f74b383e35
牛牛 <13961414+niuniu12138@user.noreply.gitee.com>
2025-02-06 16:43:59 +0800

    user

---
+++ BQ-bussiness/BQ-user/src/main/java/com/trip/app/BQUserApp.java
@@ -0,0 +1,18 @@
+package com.trip.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+
+@SpringBootApplication
+public class BQUserApp
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(BQUserApp.class,args);
+    }
+}
